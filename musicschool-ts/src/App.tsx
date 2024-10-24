0bb6aa83668225d5bfd9eeefdf37fa6ab166d98a
NekoBowl <159499381+rimomiu@users.noreply.github.com>
2024-10-24 12:20:16 -0700

    setup

---
+++ musicschool-ts/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
